7f2c627e43e10d07c2446be791df115532767ef2
Fernando de Assis <fernandoassissr@gmail.com>
2019-01-10 19:19:04 -0200

    Modelar novo teste

--- tests/cadastrarTest.php
+++ tests/cadastrarTest.php
@@ -12,10 +12,15 @@ class CadastrarTest extends \PHPUnit_Framework_TestCase
 {
     public function testCadastrarValidar()
     {
-     //    $user = 'rr@rr';        
-   		// $email = 'sddsdas';
-   		// $password = 'skdjksd';
-   		// $usuario = new User($user, $email, $password);
-     //    $this->assertTrue($usuario->insere());
+     
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
     }
 }
